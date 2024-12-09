7854b68bcd5623c6c1094d2b3ef87a2ee3992fa0
Timo Lobitz <timo.lobitz@gmail.com>
2024-12-09 13:22:13 +0100

    day09 p1 done

--- 2024/day09/src/lib.rs
+++ 2024/day09/src/lib.rs
@@ -1,38 +1,65 @@
 use std::cmp::min;
+#[derive(Debug)]
+struct File {
+    size: usize,
+    id: usize,
+}
 
 pub fn part1(input: &str) -> String {
     let input = input.trim();
 
-    let mut files: Vec<usize> = Vec::new();
+    let mut files: Vec<File> = Vec::new();
     input.chars().enumerate().for_each(|(idx, c)| {
         if idx % 2 == 0 {
-            for _ in 0..c.to_digit(10).unwrap() {
-                files.push(idx / 2);
-            }
+            let f = File {
+                size: c.to_digit(10).unwrap() as usize,
+                id: idx / 2,
+            };
+            files.push(f);
+        }
+    });
+    let mut whitespace: Vec<usize> = Vec::new();
+    input.chars().enumerate().for_each(|(idx, c)| {
+        if idx % 2 == 1 {
+            whitespace.push(c.to_digit(10).unwrap() as usize);
         }
     });
     let mut last_file = files.len() - 1;
     let mut disk: Vec<usize> = Vec::new();
-    let mut files_processed = 0;
-    input.chars().enumerate().for_each(|(idx, c)| {
-        let size = c.to_digit(10).unwrap();
 
-        if idx % 2 == 0 && files_processed < last_file {
-            for _ in 0..min(size as usize, last_file - files_processed) {
-                disk.push(idx / 2);
-                files_processed += 1;
+    let mut i = 0; // Index into files
+    let mut j = 0; // Index into whitespace
+
+    let mut index = 0;
+    while i <= last_file {
+        if index % 2 == 0 {
+            let f = &files[i];
+            for _ in 0..f.size {
+                disk.push(f.id);
             }
-        } else if idx % 2 == 1 {
-            for _ in 0..size {
-                if last_file > idx {
-                    disk.push(files[last_file]);
-                    last_file -= 1;
-                }
+            i += 1;
+            index += 1;
+        } else if index % 2 == 1 {
+            let empty = whitespace[j];
+            let f = files.get_mut(last_file).unwrap();
+            let size_to_copy = min(empty, f.size);
+            let remaining_size = f.size - size_to_copy;
+            if remaining_size == 0 {
+                last_file -= 1;
+            } else {
+                f.size = remaining_size;
+            }
+            if size_to_copy == empty {
+                index += 1;
+                j += 1;
+            } else {
+                whitespace[j] = empty - size_to_copy;
+            }
+            for _ in 0..size_to_copy {
+                disk.push(f.id);
             }
         }
-    });
-    dbg!(&disk);
-
+    }
     let checksum: usize = disk.iter().enumerate().map(|(idx, f)| idx * f).sum();
     checksum.to_string()
 }
