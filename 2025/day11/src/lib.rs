50af30b39bb800f5e57f7fe762b89d4e3d0cb318
Timo Lobitz <timo.lobitz@gmail.com>
2025-12-11 12:07:11 +0100

    part2

--- 2025/day11/src/lib.rs
+++ 2025/day11/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, hash::RandomState};
 
 use nom::{IResult, Parser, bytes::complete::tag, character::complete::{alpha1, line_ending, space1}, multi::separated_list1, sequence::separated_pair};
-use petgraph::{Graph, adj::NodeIndex, algo, graph::DiGraph};
+use petgraph::{Graph, algo::{self, toposort}, graph::DiGraph};
 
 #[derive(Debug)]
 struct Line {
@@ -49,6 +49,47 @@ pub fn part1(input: &str) -> String {
     let paths = algo::all_simple_paths::<Vec<_>, _, RandomState>(&g, start, target, 0, None).count();
     paths.to_string()
 }
+
+
+fn num_paths(src:&str, dst:&str, graph: &Graph<String,i32>, node_map :&HashMap<String,petgraph::graph::NodeIndex>) -> u128 {
+    
+    let start = *node_map.get(&src.to_string()).unwrap();
+    let target = *node_map.get(&dst.to_string()).unwrap();
+
+    let paths = count_paths_dag(graph,  start,target);
+    //println!("{} -> {} : {}",src,dst,paths);
+    paths
+}
+
+fn count_paths_dag(
+    graph: &DiGraph<String, i32>,
+    src: petgraph::graph::NodeIndex,
+    dst: petgraph::graph::NodeIndex,
+) -> u128 {
+    // Topological sort; will return Err if there is a cycle.
+    let topo = toposort(graph, None)
+        .expect("Graph is not a DAG (cycle detected)");
+
+    // dp[v] = number of paths from src to v
+    let mut dp = vec![0u128; graph.node_count()];
+    dp[src.index()] = 1;
+
+    for node in topo {
+        let ways = dp[node.index()];
+        if ways == 0 {
+            // No paths reach this node from src, skip
+            continue;
+        }
+
+        for succ in graph.neighbors_directed(node, petgraph::Direction::Outgoing) {
+            dp[succ.index()] += ways;
+        }
+    }
+
+    dp[dst.index()]
+}
+
+
 pub fn part2(input: &str) -> String {
     let input = input.trim();
     let mut g = DiGraph::<String,i32>::new();
@@ -67,48 +108,21 @@ pub fn part2(input: &str) -> String {
             g.add_edge(*src, *dst, 1);
         }
     }
-    let start = *map.get(&"svr".to_string()).unwrap();
-    let target = *map.get(&"out".to_string()).unwrap();
-    let paths = algo::all_simple_paths::<Vec<_>, _, RandomState>(&g, start, target, 0, None);
-    let mut count = 0;
-    for (i,path) in paths.enumerate() {
-        println!("{}",i);
-        if let Some(_contains_dac) = path.iter().find(|&s|  {g.node_weight(*s).unwrap()=="dac"} ) {
-            if let Some(_contains_dac) = path.iter().find(|&s|  {g.node_weight(*s).unwrap()=="fft"} ){
-                count+=1;
-            }
-        }
-    }
-    count.to_string()
-}
-
-fn num_paths(src:NodeIndex, dst:NodeIndex, graph: &Graph<String,i32>) -> usize {
-    0
-}
+    let svr_to_fft = num_paths("svr", "fft", &g, &map);
+    let fft_to_dac = num_paths("fft", "dac", &g, &map);
+    let dac_to_out = num_paths("dac", "out", &g, &map);
 
 
-pub fn part2_smart(input: &str) -> String {
-    let input = input.trim();
-    let mut g = DiGraph::<String,i32>::new();
-    let (_input, lines) = parse(input).unwrap();
-    let mut map  = HashMap::new();
+    // SVR -> FFT -> DAC -> OUT
+    let r1 = svr_to_fft * fft_to_dac * dac_to_out;
 
-    for line in lines.iter()  {
-        map.entry(line.source.clone()).or_insert(g.add_node(line.source.clone()));
-    }
-    map.entry("out".to_string()).or_insert(g.add_node("out".to_string()));
-    for line in lines.iter()  {
-        let src = map.get(&line.source).unwrap();
-        for d in line.destination.iter() {
-            let dst = map.get(d).unwrap();
+    let svr_to_dac = num_paths("svr", "dac", &g, &map);
+    let dac_to_fft = num_paths("dac", "fft", &g, &map);
+    let fft_to_out = num_paths("fft", "out", &g, &map);
+    // SVR -> DAC -> FFT -> OUT
+    let r2 = svr_to_dac * dac_to_fft * fft_to_out;
 
-            g.add_edge(*src, *dst, 1);
-        }
-    }
-    let start = *map.get(&"svr".to_string()).unwrap();
-    let target = *map.get(&"out".to_string()).unwrap();
-    num_paths(start, target, &g);
-    todo!()
+    (r1+r2).to_string()
 }
 
 #[cfg(test)]
